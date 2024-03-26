74438aedbd43663907e7beb85fdd504f468dc094
Carasinaa <113456160+Carasinaa@users.noreply.github.com>
2024-03-26 21:15:32 +0300

    Update morphs.py

--- morphs.py
+++ morphs.py
@@ -4,6 +4,8 @@ from rusyll import rusyll
 morph = MorphAnalyzer()
 rhymes = {}
 
+# достаем для слова часть речи и сохраняем в словарь
+# также я сначала думала делить слова на слоги, чтобы сделать ритмику с ударением, но ударение не получилось, а деление на слоги было убирать лень
 with open('verses.json', 'r', encoding='utf-8') as f:
     verses = json.load(f)
     for title in verses:
@@ -13,5 +15,6 @@ with open('verses.json', 'r', encoding='utf-8') as f:
             pos = ana.tag.POS
             syllables = rusyll.word_to_syllables_wd(word)
             rhymes[word] = (pos, syllables)
+# сохраняем словарь в джисоновский файл
 with open('morphs.json', 'w', encoding='utf-8') as f:
   json.dump(rhymes, f, ensure_ascii = False)
